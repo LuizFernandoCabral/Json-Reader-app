d7e33fd25383cb11f0053b92caf51bedea44d4a1
Luiz Cabral <Luiz Cabral>
2016-08-29 05:07:58 -0300

    Changed table to linearlayout

--- app/src/main/java/luiz/exeestagio/MainActivity.java
+++ app/src/main/java/luiz/exeestagio/MainActivity.java
@@ -3,39 +3,34 @@ package luiz.exeestagio;
 import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.JsonReader;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TableLayout;
+import android.widget.LinearLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.json.simple.parser.JSONParser;
 
-import java.io.BufferedReader;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 
     protected JSONObject inFile;
-    protected TableLayout users;
+    protected LinearLayout users;
     private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        users = (TableLayout) findViewById(R.id.Users);
+        users = (LinearLayout) findViewById(R.id.Users);
     }
 
     public void newFile (View v) {
         JSONArray data;
-        JSONParser parser = new JSONParser();
-        String id, name, pwd;
+        String id, name, pwd, json;
         TextView ID, Name, Pwd;
         EditText FileName = (EditText) findViewById(R.id.FileName);
 
@@ -44,36 +39,47 @@ public class MainActivity extends AppCompatActivity {
         try {
             AssetManager assetManager = getAssets();
             InputStream reader = assetManager.open(FileName.getText().toString());
-            BufferedReader buffer = new BufferedReader(new InputStreamReader(reader));
-            JsonReader JReader = new JsonReader(buffer);
+            byte[] buffer = new byte[reader.available()];
+            reader.read(buffer);
+            reader.close();
+            json = new String(buffer, "UTF-8");
 
-            inFile = new JSONObject(JReader.toString());
+            Log.d(TAG, json);
+
+            //inFile = JReader.readObject();
+
+            inFile = new JSONObject(json);
             data = inFile.getJSONArray("data");
 
             FileName.setText("");
             for (int i = 0; i < data.length(); i++) {
-                TableRow user = new TableRow(this);
-                id = data.getJSONObject(i).getString("id");
-                name = data.getJSONObject(i).getString("name");
-                pwd = data.getJSONObject(i).getString("pwd");
+                LinearLayout user = new LinearLayout(this);
+                id = "ID: ";
+                name = "Name: ";
+                pwd = "PWD: ";
+                id += data.getJSONObject(i).getString("id");
+                name += data.getJSONObject(i).getString("name");
+                pwd += data.getJSONObject(i).getString("pwd");
 
-                Log.d(TAG, "passou");
+                id += "\n";
+                name += "\n";
+                pwd += "\n";
 
                 ID = new TextView(this);
                 Name = new TextView(this);
                 Pwd = new TextView(this);
 
-                ID.setText("ID: " + id);
-                Name.setText("Name: " + name);
-                Pwd.setText("PWD: " + pwd);
+                ID.setText(id);
+                Name.setText(name);
+                Pwd.setText(pwd);
 
                 user.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT));
+                user.setMinimumHeight(100);
                 user.addView(ID);
                 user.addView(Name);
                 user.addView(Pwd);
 
-                user.addView(user);
-
+                users.addView(user);
             }
         }catch (Exception e) {
             Log.d(TAG, e.getMessage());
